3edaf3034c1224f45ebdd9403d49a7de0a807309
HeftySammich <138090485+HeftySammich@users.noreply.github.com>
2025-05-03 10:54:21 -0400

    Removed duplicate index.tsx and tasks.tsx from root directory and cleaned up files not being used.

--- index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{fontFamily:'sans-serif',textAlign:'center',marginTop:'3rem'}}>
-      <h1>Bookmarq</h1>
-      <p>Connect your Twitter account and start turning bookmarks into on‑chain tasks.</p>
-      <a href="/tasks">Go to tasks</a>
-    </main>
-  );
-}
